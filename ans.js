c3d17fad86ee26a18d25498829ad02e474963d87
Abhay Pratap Singh <abhay.pratap_cs23@gla.ac.in>
2025-03-15 13:25:10 +0530

    Add files via upload

---
+++ ans.js
@@ -0,0 +1,10 @@
+console.log("Connection successful");
+
+function yes() {
+    alert("I Love You Too..");
+    // document.getElementById("no1").style.display = "block";
+}
+
+function no() {
+    alert("Really??")
+}
